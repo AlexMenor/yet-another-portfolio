f184cba5d236cc670fa2826497f5ea0635947f50
Alex Menor <alex.menor@icloud.com>
2021-08-22 17:45:01 +0200

    improve copys

--- sections/stack.js
+++ sections/stack.js
@@ -10,29 +10,29 @@ export default function Stack() {
     {
       name: "Frontend",
       description:
-        "I've worked a lot with Vue and React as SPA's and server rendered. I also like a lot to develop mobile applications for what I've used React native and Flutter. <br/><br/> \
-				Strong HTML/CSS/JS is also important for me, and for medium and large projects I usually setup Typescript. <br/>",
+        "I've worked a lot with Vue and React as SPA's and server rendered. I also like to develop mobile apps, for what I've used React Native and Flutter. <br/><br/> \
+				Strong HTML/CSS/JS knowledge is also important for me, and for medium and large projects I usually setup Typescript. <br/>",
       picture: frontPic,
     },
     {
       name: "Backend",
       description:
-        "In the server side of things, robust and clean code is the most important thing for me, applying DDD for clean architectures when it's necessary. <br/><br/> \
+        "In the server side of things, robust and clean code is the most important thing for me, applying DDD & clean architecture when it's necessary. <br/><br/> \
 				Language-wise I like Typescript a lot but I've also worked with Python and Go. In all of them I've used opinionated and unopinionated frameworks.",
       picture: backPic,
     },
     {
       name: "Databases",
       description:
-        "I've been in projects of all sorts with different data needs. PostgreSQL has never let me down but I've also used MongoDB for some project with no relational data. <br/><br/>\
-				Redis has also been an important tool in my belt for caching, scaling websockets or even as the main database.",
+        "I've been in projects of all sorts with different data needs. PostgreSQL has never let me down but I've also used MongoDB for some domains with no relational data. <br/><br/>\
+				Redis has also been an important tool in my belt for caching and scaling websockets, for example.",
       picture: dbPic,
     },
     {
       name: "Cloud",
       description:
-        "I try not to make things hard at deployment time if it's not necessary, but I do consider important to have a proper CI/CD pipeline for most projects. Github actions is my favourite \
-				but I used CircleCI and travis too. <br/><br/>In many of my projects I've been on my own, so I had to configure the infraestructure in cloud providers such as AWS or GCP. When maximum \
+        "I try not to make things hard at deployment time if it's not necessary, but I do consider important to have a proper CI/CD pipeline for most projects. Github actions is my favourite for this\
+				but I've used CircleCI and travis too. <br/><br/>In many of the projects I've worked in I had to configure the infraestructure in cloud providers such as AWS or GCP. When maximum \
 				flexibility and scalability is required, Kubernetes has been helpful for me.",
       picture: cloudPic,
     },
@@ -44,7 +44,7 @@ export default function Stack() {
     >
       <h1 className="font-display text-5xl">My Stack</h1>
       <p className="text-xl my-5">
-        I've always tried to be stack agnostic and spent more time learning
+        I've always tried to be stack agnostic and spend more time learning
         about fundamentals and patterns that make great applications.
         <br />
         <br /> These are the technologies I have more experience with, but I
--- sections/story.js
+++ sections/story.js
@@ -4,6 +4,7 @@ import ideasFactory from "../public/assets/ideasfactory.jpeg";
 import routeleo from "../public/assets/routeleo.jpeg";
 import pantala from "../public/assets/pantala.jpg";
 import bounsel from "../public/assets/bounsel.png";
+import internxt from "../public/assets/internxt.jpeg";
 
 export default function Story() {
   const items = [
@@ -18,22 +19,31 @@ export default function Story() {
       year: "2019",
       title: "Many adventures",
       paragraph:
-        "Being around people better than me pushed me to learn a lot by myself. Web development became my #1 obsession.<br/><br/>I also got to apply these new skills to many projects: <br/>Our goal in Vodafone Campus Lab was to help palliate the loneliness of elderly people.<br/>Kanuki was a content platform, we provide the space and tools so the creators can share their talent with the world.<br/>In Routeleo we fulfill the dream of travellers on a budget. Discover the way to get more for less and be able to get to the best places in the world.",
+      "Being around people better than me pushed me to learn a lot by myself. Web development became my #1 obsession and I got to apply these new skills in many projects.<br/> <br/>For example Vodafone Campus Lab, in which our goal was to help palliate the loneliness of elderly people.<br/>Other project was Kanuki, a content platform where we provided the space and tools for the creators to share their talent with the world.<br/>To end the year, in Routeleo we worked to allow travellers on a budget get to the best places in the world for less money.<br/><br/> I learnt a lot working on all these projects with people from very different backgrounds.",
       picture: routeleo,
     },
     {
       year: "2020",
       title: "My first job",
       paragraph:
-        "My prior experience allowed me to get a position in an early-stage startup called Pantala, which is an unlimited closet as a service. <br/>In a world where fast fashion is a thing, we wanted our customers to have choice and joy for what they wear while being friendly to our planet.<br/><br/>We had three months to get a seed round and we needed to build a system from scratch that allowed us operate. <br/><br/>After that, I remained working as Full Stack Developer.",
+        "My prior experience allowed me to get a position in an early-stage startup called Pantala, an unlimited closet as a service. In a world where fast fashion is a thing, we wanted our customers to have great clothes while being friendly to our planet.<br/><br/>We had three months to get a seed round and we needed to build a system from scratch that allowed us to operate. <br/><br/>After that, I remained working as a Full Stack Developer there. It was a very important experience for me as I had to take decisions and work under pressure many times.",
       picture: pantala,
     },
     {
       year: "2021",
       title: "Closing the circle",
       paragraph:
-        "In the middle of my last year in college I got an offer to move to Valencia and join Bounsel, an ambitious project that aims to end every pain in contract management with an all-in-one platform.<br/><br/>I also finished my degree, having “Héroes” as the final project. <br/><br/>Finally, I decided to join another startup called Internxt, that makes great privacy-first software.",
+        "In the middle of my last year of college I got an offer to move to Valencia and join Bounsel, an ambitious project that aims to end every pain in contract management with an all-in-one platform.<br/><br/>I got to expand my technical skills a lot as it is a hard product to build, and had the luck of working with brilliant people.<br/><br/>I also finished my degree, having “Héroes” as the final project.",
       picture: bounsel,
+    },
+    {
+      year: "",
+      title: "What am I doing now?",
+      paragraph:
+        "I've always believed that privacy should be in the core of every product. Software built to steal our attention and that we pay with our data is harmful. <br/><br/>\
+        For that, I'm so happy to work at Internxt, where we build privacy-first software in which you can trust and that serves you (and not the other way around). <br/><br/>\
+        I'm also investing time learning about complementary disciplines to mine such as Bussiness and Design.",
+      picture: internxt,
     },
   ];
   return (
