85fa19a772755f86bc009dbb2a00669c89f7922d
Alex Menor <alex.menor@icloud.com>
2021-09-12 12:07:29 +0200

    add meta stuff

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,58 @@ import Landing from "../sections/landing";
 import Story from "../sections/story";
 import Stack from "../sections/stack";
 import Social from "../components/Social";
+import metaImage from "../public/meta-image.png";
 
 export default function Home() {
   return (
     <div className="bg-white">
       <Head>
         <title>Alejandro Menor</title>
+        <meta name="title" content="Alejandro Menor" />
+        <meta
+          name="description"
+          content="I'm a Full Stack Developer from Spain that likes to work on impactful projects"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.alexmenor.es/" />
+        <meta property="og:title" content="Alejandro Menor" />
+        <meta
+          property="og:description"
+          content="I'm a Full Stack Developer from Spain that likes to work on impactful projects"
+        />
+        <meta property="og:image" content={metaImage.src} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.alexmenor.es/" />
+        <meta property="twitter:title" content="Alejandro Menor" />
+        <meta
+          property="twitter:description"
+          content="I'm a Full Stack Developer from Spain that likes to work on impactful projects"
+        />
+        <meta property="twitter:image" content={metaImage.src} />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        ></link>
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        ></link>
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+
+        <link rel="manifest" href="/site.webmanifest"></link>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
