287a43d97d375bf3875a96ae9dd585e6d88c097f
Alex Menor <alex.menor@icloud.com>
2021-12-06 12:31:29 +0100

    use next builtin font optimization

--- pages/_document.js
+++ pages/_document.js
@@ -28,12 +28,6 @@ class MyDocument extends Document {
 
           <link rel="manifest" href="/site.webmanifest"></link>
           <link rel="icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
           <link
             href="https://fonts.googleapis.com/css2?family=Martel:wght@900&display=swap"
             rel="stylesheet"
