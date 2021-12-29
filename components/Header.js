eab7f7049801bb026c2534b190fa77f33d50079f
Alex Menor <alex.menor@icloud.com>
2021-12-29 17:28:45 +0100

    improve footer

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Header({ mode = 'dark' }) {
   return (
     <header
-      className={`px-5 md:px-8 pt-5 md:pt-7 lg:px-14 flex justify-between w-full absolute top-0 z-10 `}
+      className={`px-5 md:px-8 pt-5 md:pt-7 lg:px-14 flex justify-between w-full absolute top-0 z-10`}
     >
       <div className="flex items-center justify-between md:space-x-8 md:justify-start w-full">
         <Link href="/" passHref>
