e3dd066f94ac905533afb74062f3c0a3ea823b2d
Alex Menor <alex.menor@icloud.com>
2022-01-19 20:29:35 +0100

    add emoji to posts index

--- components/PostsHero.js
+++ components/PostsHero.js
@@ -6,7 +6,7 @@ export default function PostsHero({ className = '' }) {
       className={`text-center py-6 md:py-12 ${className}`}
     >
       <h1 className="font-display text-3xl md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r from-secondary/80 to-secondary/90 bg-black">
-        My most recent posts
+        <span className="text-black">📝&nbsp;</span>My most recent posts
       </h1>
       <p className="mt-5 px-10 text-gray-600 text-base md:text-lg font-light">
         Writing is a powerful tool to clarify thoughts and lessons
